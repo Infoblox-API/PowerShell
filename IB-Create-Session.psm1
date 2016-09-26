890e6f18622e574517f41afd8835574ce12480f2
Don Smith <Ameritus@users.noreply.github.com>
2016-09-26 17:24:32 -0400

    Update IB-Create-Session.psm1

--- IB-Create-Session.psm1
+++ IB-Create-Session.psm1
@@ -21,9 +21,43 @@ function script:IB-Create-Session {
     .SYNOPSIS
         Establishes a session to an Infoblox Grid Master for all subsequent data calls.
         
+    .DESCRIPTION
+        Creates a connection to an Infoblox Grid Master and leaves open an active session.  Defines several global variables for subsequent use.
+        
+        ib_grid_name            The name of the Grid (for multi Grid environments -- like Test/Dev vs. Production)
+        ib_grid_master          The IP or hostname of the Grid we are connecting to
+        ib_grid_ref             A reference to the Grid object
+        ib_session              The WebSession variable
+        ib_uri_base             "https://$grid_master/wapi/$wapi_ver"
+        ib_username             The admin name
+        ib_wapi_ver             The WAPI version string used to connect
+        
     .PARAMETER grid_master 
         A single computer name or IP address.
         
+    .PARAMETER username
+        Authenticating user; defaults to "admin"
+        
+    .PARAMETER password
+        Admin password.  If not entered, use the -ask_pw option.
+        
+    .PARAMETER wapi_ver
+        Use to specify the exact version of the WAPI to use.
+        The version must be in "v#.#" format and must be supported by the installed version of NIOS.
+        Defaults to "v1.3"
+ 
+    .PARAMETER max_results
+        Determines the maximum number of results to be returned.
+        Enter as a whole number ("10000").
+        Defaults to 10,000.
+        
+    .PARAMETER ask_pw
+        Forces a prompt for inputting the password manually so it does not have to be entered in clear text on the command line.
+    
+    .OUTPUTS
+        Optional connection message (Grid name you are connected to)
+        TRUE or FALSE depending on the connection status
+    
     .EXAMPLE
         IB-Create-Session 192.168.1.2 admin infoblox
         Creates a connection to a Grid Master using default credentials and IP address.
