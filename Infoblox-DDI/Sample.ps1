5e9d5c2ce42ed511ad8ddb09fa469cb9835fd3d7
Don Smith <smithdonalde@gmail.com>
2016-09-28 14:32:05 -0400

    Schema and unit test updates
    Can now get latest Wapi version from schema.

--- Infoblox-DDI/Sample.ps1
+++ Infoblox-DDI/Sample.ps1
@@ -1,25 +1,35 @@
+# Remove the module if loaded so we can reload it
+Get-Module Infoblox-DDI | Remove-Module
+
+# Load the current module
 Import-Module .\Infoblox-DDI.psd1
 Write-Host ""
 
 $GridMaster = Read-Host "Grid Master"
 $Username   = Read-Host "Username"
 
-Connect-IBGridMaster $GridMaster $Username -ask -force -Debug
+Connect-IBGridMaster $GridMaster $Username -ask -force
 
 Show-IBSessionVariables
 Write-Host ""
 
 # Fail tests
+Write-Host "Fail Tests"
 Set-IBMaxResults 0
 Set-IBMaxResults 1000001
-Set-IBWapiVersion "2.3.1"
-Set-IBWapiVersion "v 2.3.1"
+Set-IBWapiVersion "1.7"
+Set-IBWapiVersion "v 1.7"
 
 # Success tests
+Write-Host "Success Tests"
 Set-IBMaxResults 2000
-Set-IBWapiVersion "v2.3.1"
+Set-IBWapiVersion "v1.7"
+Set-IBWapiVersion
 
+Write-Host "Find network 192.168.1.0"
 Find-IBNetwork 192.168.1.0 | Format-Table
 
-$test_data = Get-IBNetwork network/ZG5zLm5ldHdvcmskMTkyLjE2OC4xLjAvMjQvMA:192.168.1.0/24/Company%201 -json -Debug
+Write-Host "Get network with ref"
+#$test_data = Get-IBNetwork network/ZG5zLm5ldHdvcmskMTkyLjE2OC4xLjAvMjQvMA:192.168.1.0/24/Company%201 -json
 #$test_data
+
