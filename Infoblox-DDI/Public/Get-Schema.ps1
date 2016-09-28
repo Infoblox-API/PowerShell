7fcac9c81ba18a90920080b5507329f672200dbf
Don Smith <smithdonalde@gmail.com>
2016-09-28 14:30:39 -0400

    Schema retrieval
    Added function to get the schema from the Grid

---
+++ Infoblox-DDI/Public/Get-Schema.ps1
@@ -0,0 +1,40 @@
+<#
+	.SYNOPSIS
+		Retrieves the schema object from the Grid and displays it
+		
+	.DESCRIPTION
+		Retrieves the schema object from the Grid and displays it
+		
+	.OUTPUTS
+		Shows schema data from the Grid
+
+	.EXAMPLE
+		Show-Schema
+		
+#>
+
+function script:Get-Schema {
+    BEGIN {
+    }
+
+    PROCESS {
+	}
+
+    END {
+        # Set the URI to retrieve the Grid object
+        $uri = "https://$script:ib_grid_master/wapi/v1.0/?_schema"
+        Write-Debug "[DEBUG] [Get-Schema] URI = $uri"
+
+		# Make a connection to the Grid and print the detailed error message as necessary
+        try {
+            $schema_obj = Invoke-RestMethod -Uri $uri -Method Get -WebSession $script:ib_session
+        } catch {
+            Write-Host '[ERROR] There was an error retrieving the schema.'
+            Write-Host $_.ErrorDetails
+            return $false
+        }
+
+        Write-Debug "[DEBUG] [Get-Schema] $schema_obj"
+        return $schema_obj
+    }
+}
